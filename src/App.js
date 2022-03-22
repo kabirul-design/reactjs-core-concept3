c9cefc2602db8b0d7ffab4f40301aa2900c4109f
kabirulpHero <kabirulislamuo@gmail.com>
2022-03-22 22:47:55 +0600

    core concept

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+      <LoadComment></LoadComment>
+     
     </div>
   );
+}
+// useEffect( ()=>{},[])
+
+function LoadComment(){
+  const [comments, setComments] = useState([]);
+
+  fetch('https://jsonplaceholder.typicode.com/comments')
+  .then(res => res.json())
+  .then(data => setComments(data))
+
+  return(
+    <div>
+      <h3> Comments : {comments.length}</h3>
+      {
+        comments.map(comment => <li name={comment.name}></li>)
+      }
+    </div>
+  )
+}
+
+
+
+function Counter(){
+  const [count, setCount] = useState(0)
+  const handleIncreses = () =>setCount(count + 1);
+  const handleDecreses = () => setCount(count - 1);
+
+  return(
+    <div>
+      <h1>Counter : {count} </h1>
+      <button onClick={handleIncreses}>Increase</button>
+      <button onClick={handleDecreses}>Decrease</button>
+    </div>
+  )
 }
 
 export default App;
